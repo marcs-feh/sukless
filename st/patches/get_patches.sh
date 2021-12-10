068e19022ea26071ce5c3c8410d19b05d8d68cf1
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 12:22:53 -0300

    st: get patches

---
+++ st/patches/get_patches.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget \
+https://st.suckless.org/patches/alpha/st-alpha-0.8.2.diff \
+https://st.suckless.org/patches/anysize/st-anysize-0.8.4.diff \
+https://st.suckless.org/patches/boxdraw/st-boxdraw_v2-0.8.3.diff \
+https://st.suckless.org/patches/defaultfontsize/st-defaultfontsize-20210225-4ef0cbd.diff \
+https://st.suckless.org/patches/font2/st-font2-20190416-ba72400.diff \
+https://st.suckless.org/patches/csi_22_23/st-csi_22_23-0.8.4.diff \
+https://st.suckless.org/patches/delkey/st-delkey-20201112-4ef0cbd.diff \
+https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff
