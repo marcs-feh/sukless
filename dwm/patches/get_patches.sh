4a3fbdcf818711313c4abd376da941d110986f9a
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 19:25:33 -0300

    dwm: fetch

---
+++ dwm/patches/get_patches.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+wget \
+https://dwm.suckless.org/patches/ewmhtags/dwm-ewmhtags-6.2.diff \
+https://dwm.suckless.org/patches/centeredmaster/dwm-centeredmaster-20160719-56a31dc.diff \
+https://dwm.suckless.org/patches/movestack/dwm-movestack-20211115-a786211.diff \
+https://dwm.suckless.org/patches/pertag/dwm-pertag-20200914-61bb8b2.diff \
+https://dwm.suckless.org/patches/attachbelow/dwm-attachbelow-6.2.diff \
+https://dwm.suckless.org/patches/bar_height/dwm-bar-height-6.2.diff \
+https://dwm.suckless.org/patches/alwaysontop/alwaysontop-6.2.diff \
+https://dwm.suckless.org/patches/alwayscenter/dwm-alwayscenter-20200625-f04cac6.diff \
+https://dwm.suckless.org/patches/actualfullscreen/dwm-actualfullscreen-20211013-cb3f58a.diff \
+https://dwm.suckless.org/patches/colorbar/dwm-colorbar-6.2.diff \
+https://dwm.suckless.org/patches/fibonacci/dwm-fibonacci-20200418-c82db69.diff \
+https://github.com/mihirlad55/dwm-anybar/releases/download/v1.1.2/dwm-anybar-20210926-a786211.diff \
