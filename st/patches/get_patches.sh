d0cd5091bc198fae40c907be0835e5cfe5c9f6fb
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 12:33:07 -0300

    st: patch scrollback

--- st/patches/get_patches.sh
+++ st/patches/get_patches.sh
@@ -8,4 +8,6 @@ https://st.suckless.org/patches/defaultfontsize/st-defaultfontsize-20210225-4ef0
 https://st.suckless.org/patches/font2/st-font2-20190416-ba72400.diff \
 https://st.suckless.org/patches/csi_22_23/st-csi_22_23-0.8.4.diff \
 https://st.suckless.org/patches/delkey/st-delkey-20201112-4ef0cbd.diff \
+https://st.suckless.org/patches/scrollback/st-scrollback-0.8.4.diff \
+https://st.suckless.org/patches/scrollback/st-scrollback-mouse-20191024-a2c479c.diff \
 https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff
