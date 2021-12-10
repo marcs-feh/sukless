ca2e9a37a333484100f15b417b8990b1cfb1a08c
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 12:44:37 -0300

    st: patch nordtheme

--- st/config.h
+++ st/config.h
@@ -116,32 +116,26 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	[0]="#3b4252", /* black   */
+	[1]="#bf616a", /* red     */
+	[2]="#a3be8c", /* green   */
+	[3]="#ebcb8b", /* yellow  */
+	[4]="#81a1c1", /* blue    */
+	[5]="#b48ead", /* magenta */
+	[6]="#88c0d0", /* cyan    */
+	[7]="#e5e9f0", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]="#4c566a", /* black   */
+	[9]="#bf616a", /* red     */
+	[10]="#a3be8c", /* green   */
+	[11]="#ebcb8b", /* yellow  */
+	[12]="#81a1c1", /* blue    */
+	[13]="#b48ead", /* magenta */
+	[14]="#8fbcbb", /* cyan    */
+	[15]="#eceff4", /* white   */
 
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+	[256]="#2e3440", /* background */
+	[257]="#d8dee9", /* foreground */
 };
 
 
@@ -149,10 +143,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 0;
 
 /*
  * Default shape of cursor
@@ -161,7 +155,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
