1a326e18d1483df37affacde21434e92536994ad
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 19:57:57 -0300

    dwm: patch barheight + conflict fix
    Fixed conflict between anybar and barheight, removed ternary operator in
    favor of a saner if-else statement.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,7 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
+static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const char *altbarclass      = "Polybar";/* Alternate bar class name */
 static const char *altbarcmd        = "$HOME/bar.sh"; /* Alternate bar launch command */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
