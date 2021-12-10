30ce545f28ca73673fa6c9435323c88aa95b5e7d
marcs-feh <marcosfehlauer@protonmail.com>
2021-12-10 15:30:42 -0300

    dmenu: fetch

---
+++ dmenu/patches/get_patches.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+wget \
+https://tools.suckless.org/dmenu/patches/case-insensitive/dmenu-caseinsensitive-20200523-db6093f.diff \
+https://tools.suckless.org/dmenu/patches/xyw/dmenu-xyw-5.0.diff \
+https://tools.suckless.org/dmenu/patches/line-height/dmenu-lineheight-5.0.diff \
+https://tools.suckless.org/dmenu/patches/lines-below-prompt/dmenu-linesbelowprompt-and-fullwidth-20211014.diff \
+https://tools.suckless.org/dmenu/patches/dynamicoptions/dmenu-dynamicoptions-5.0.diff \
+https://tools.suckless.org/dmenu/patches/grid/dmenu-grid-4.9.diff \
+https://tools.suckless.org/dmenu/patches/gridnav/dmenu-gridnav-5.0.diff \
+https://tools.suckless.org/dmenu/patches/mouse-support/dmenu-mousesupportwithgrid-5.0.diff \
