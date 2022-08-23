68bea23ebddcda475fd355b00196f5cc78ad1f25
marcs-feh <marcosfehlauer@protonmail.com>
2022-08-23 14:38:12 -0300

    add ligature support

--- st/patches/get_patches.sh
+++ st/patches/get_patches.sh
@@ -9,4 +9,5 @@ https://st.suckless.org/patches/font2/st-font2-20190416-ba72400.diff \
 https://st.suckless.org/patches/csi_22_23/st-csi_22_23-0.8.4.diff \
 https://st.suckless.org/patches/scrollback/st-scrollback-0.8.4.diff \
 https://st.suckless.org/patches/scrollback/st-scrollback-mouse-20191024-a2c479c.diff \
-https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff
+https://st.suckless.org/patches/nordtheme/st-nordtheme-0.8.2.diff \
+https://st.suckless.org/patches/ligatures/0.8.4/st-ligatures-boxdraw-20210824-0.8.4.diff
