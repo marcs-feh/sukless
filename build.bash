04a4c908de95349103baa08d6cc246e60851c8b5
marcs-feh <marcosfehlauer@protonmail.com>
2023-09-08 17:20:27 -0300

    change build step to support prefix and fix slock bs

--- build.bash
+++ build.bash
@@ -2,27 +2,39 @@
 
 programs=('dwm' 'st' 'dmenu' 'slock' 'dwmblocks')
 
-Build() { make; }
-Install() { make install; }
-Clean() { make clean; }
-Uninstall() { make uninstall; }
-Help() {
-	echo 'build.bash [build|install|uninstall|clean]'
-	echo '    Use `PREFIX="your_prefix" ./build.bash` to install to another location'
-	exit 1
-}
+Build='make'
+Install='make install'
+Clean='make clean'
+Uninstall='make uninstall'
+Help='build.bash [build|install|uninstall|clean]\n    Use `PREFIX="your_prefix" ./build.bash` to install to another location'
 
-action="$1"
-case $action in
-	'build') action=Build;;
-	'install') action=Install;;
-	'uninstall') action=Uninstall;;
-	'clean') action=Clean;;
-	*) action=Help ;;
+Action="$1"
+case $Action in
+	'build')     Action="$Build";;
+	'install')   Action="$Install";;
+	'uninstall') Action="$Uninstall";;
+	'clean')     Action="$Clean";;
+	*)           printf "$Help\n"; exit 1;;
 esac
 
+
 for p in ${programs[@]}; do
+
 	cd $p
-	$action
+
+	if [ "$p" = "slock" ]; then
+		if [ ! "$(whoami)" = "root" ]; then
+			[ -z "$(which sudo)" ] \
+				&& Doer=doas \
+				|| Doer=sudo
+
+				echo "WARNING: slock requires root to be built as it needs suid, using $Doer to aquire privilege"
+			$Doer $Action
+		fi
+		$Action
+	else
+		$Action
+	fi
+
 	cd ..
 done
